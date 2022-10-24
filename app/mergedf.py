8114b11eea4ee44863b1c98f250195253d3ce2f2
chrisVazquezgo <christian.vazquez@grupotimexico.com>
2022-10-24 11:58:28 -0500

    revision
    final

--- app/mergedf.py
+++ app/mergedf.py
@@ -16,28 +16,34 @@ class VerifyData:
         df3.loc[lambda x: x['_merge'] == 'both']
         result_data = df3[(df3['_merge'] != 'both')].drop('_merge', axis=1)
 
-        
         result = pd.DataFrame(result_data)
-        result.to_csv('reporte_incidencias.csv')
 
         ejecucion = datetime.now()
         ejecucion_f = ejecucion.strftime('%d/%m/%Y %H:%M:%S')
 
         # page 1 to excel
         number = f' Se encontraron el numero {len(result)} de datos incongruentes'
+        number2 = f' Se encontraron el numero 0 de datos incongruentes'
         second = f'fecha: {ejecucion_f}'
 
         if (len(result) == 0):
-            page1 = pd.DataFrame({"Numero de registros": (number, second)})
+            TOTAL = pd.DataFrame({"Informacion de datos": (
+                "No se encontraron datos diferentes", "En los dos dataframes")})
+            TOTAL.to_csv('reporte_incidencias.csv')
+
+            page1 = pd.DataFrame({"Numero de registros": (number2, second)})
             page2 = pd.DataFrame({"Informacion de datos": (
-                "No se encontraron datos diferentes", "En los dos dataframes")},index=False)
+                "No se encontraron datos diferentes", "En los dos dataframes")})
 
             with ExcelWriter('prueba.xlsx') as writer:
                 page1.to_excel(writer, 'PAGE 1', index=False)
                 page2.to_excel(writer, 'PAGE 2', index=False)
         else:
+
+            result.to_csv('reporte_incidencias.csv')
+
             page1 = pd.DataFrame({"Numero de registros": (number, second)})
-            page2 = pd.DataFrame(result.tail(4))
+            page2 = pd.DataFrame(result.tail(5))
 
             with ExcelWriter('prueba.xlsx') as writer:
                 page1.to_excel(writer, 'PAGE 1', index=False)
